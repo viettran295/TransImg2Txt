e3f476a195c5af135c46c84982500a4d8e5300b0
viettr <viettran295@gmail.com>
2024-03-02 20:43:45 +0100

    Add write csv func and process string before writing

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 import easyocr 
+import csv 
+import re
 
-def TransImg2Txt(imgPath: str) -> str:
+def WriteCsv(filename: str, trans: list[str]):
+    """
+    Write list of string to csv file
+    """
+    trans.insert(0, ["Date", "State", "Symbol", "Amount", "Price value"])
+    with open(filename, 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(trans)
+    
+def TransImg2Txt(imgPath: str) -> list[list[str]]:
     """
     Convert transaction image from Scalable trading platform to txt
     """
     reader = easyocr.Reader(['en'])
     res = reader.readtext(imgPath)
-    txt = ""
+    txt = []
+    tmp = []
+    col_idx = 0
     for i in range(len(res)):
         char = res[i][1]
-        txt += char + ', '
-        if '€' in char:
-            txt += "\n"
+        char = re.sub(';+', '', char)
+        tmp.append(char)
+        col_idx += 1
+        if col_idx == 5:
+            txt.append(tmp.copy())
+            tmp.clear()
+            col_idx = 0
     return txt
 
 if __name__ == "__main__":
-    imgPath = './img/transactions.png'
-    print(TransImg2Txt(imgPath))
+    imgPath = './img/mstr2.png'
+    trans = TransImg2Txt(imgPath)
+    print(trans)
+    WriteCsv('transaction.csv', trans)
