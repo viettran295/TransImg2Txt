257bb35da8039aff4348f97bfa91a435829d64a0
viettr <viettran295@gmail.com>
2024-03-24 14:55:37 +0100

    Sum value with the same category

--- processor/preprocess.py
+++ processor/preprocess.py
@@ -1,4 +1,5 @@
 import polars as pl 
+from utils.error_handler import logger
 
 def concate_df(dfs: list[pl.DataFrame]) -> pl.DataFrame:
     """
@@ -8,3 +9,16 @@ def concate_df(dfs: list[pl.DataFrame]) -> pl.DataFrame:
     for df in dfs:
         df_concat = pl.concat([df_concat, df], how='vertical')
     return df_concat
+
+def sum_w_condition(df: pl.DataFrame, sum_col: str, state="Kauf") -> pl.DataFrame:
+    """
+    Sum vertically with the condition: "State" is equal to "Kauf" or "Verkauf"
+    """
+    tmp = df.select(
+                pl.col(sum_col).filter(pl.col("State") == state).sum()
+                )
+    df = df.with_columns(
+                pl.lit(tmp).alias(f"Sum_{sum_col}_{state}")
+                )
+
+    return df
